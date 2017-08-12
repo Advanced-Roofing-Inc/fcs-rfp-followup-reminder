530fba1360af5bafb1b11725ea01bd39bfa25724
Dylan Foster <dylantf@gmail.com>
2017-08-12 12:23:49 -0400

    add config, test mailer. proper query

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 const mysql = require('mysql');
 const moment = require('moment');
-
+const config = require('./config.json');
+const he = require('he');
+const nodemailer = require('nodemailer');
 
 const dateFormat = 'YYYY-MM-DD 00:00:00';
-const today = moment();
-const yesterday = moment(today).add(-1, 'days');
-console.log('Today is:', today.format(dateFormat));
-console.log('Yesterday is:', yesterday.format(dateFormat));
+const today = moment('2017-07-17');
+const targetDates = [];
 
-process.exit();
+try {
+  config.rfpAges.forEach(numDays => {
+    const dateString = moment(today).subtract(numDays, 'days').format(dateFormat);
+    targetDates.push(dateString);
+  });
+} catch (err) {
+  console.error('Error reading config property `rfpAges`.');
+  process.exit(1);
+}
 
-////////
-
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'app_contractors'
-});
+// Connect to the mysql database
+console.log('Connecting to MySQL...');
+const conn = mysql.createConnection(config.database);
 
 conn.connect((err) => {
   if (err) {
@@ -26,12 +29,36 @@ conn.connect((err) => {
   }
 });
 
-conn.query('select * from OpenQuotes limit 10', (error, results, fields) => {
+const sql = 'SELECT * FROM `OpenQuotes` WHERE `creation_date` IN (?)';
+conn.query(sql, [targetDates], (error, results, fields) => {
+  if (error) {
+    console.error('Error querying database:', error);
+    process.exit(1);
+    return;
+  }
+  
   results.forEach(result => {
-    console.log(result['site_name']);
+    const creationDate = moment(result['creation_date']).format('MM/DD/YY');
+    console.log(creationDate, he.decode(result['site_name']));
   });
 });
 
 conn.end();
 
-// process.exit();
+// Email mock
+const transporter = nodemailer.createTransport(config.email);
+
+const mailOptions = {
+  from: 'Dylan Tester',
+  to: 'Dylan Test <dylan@dylantest.com>',
+  subject: 'Email test',
+  text: 'This is plain text email',
+  html: '<h1>This is html email</h1>'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log('Mail sent:', info);
+});
